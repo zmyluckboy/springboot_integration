bd2529e51c44020969ebea5c0939402ef378fb2b
zhaomy <zmyluckboy@163.com>
2020-04-30 00:28:28 +0800

    springboot集成redis之框架搭建

---
+++ src/main/java/com/huaweisoft/springboot_integration/SpringbootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.huaweisoft.springboot_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huaweisoft/springboot_integration/SpringbootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaweisoft.springboot_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
